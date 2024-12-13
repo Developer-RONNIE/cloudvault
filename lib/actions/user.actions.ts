a0baf3f041bebeeb2628438b6e845c591d349dd9
Developer-Ronnie <ronnieworkspace@gmail.com>
2024-12-13 10:39:11 +0530

    Added sign out User functionality

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -6,6 +6,7 @@ import { ID, Query } from "node-appwrite";
 import { parseStringify } from "@/lib/utils";
 import { cookies } from "next/headers";
 import { avatarPlaceholderUrl } from "@/constants";
+import { redirect } from "next/navigation";
 
 
 const getUserByEmail = async (email: string) => {
@@ -112,4 +113,18 @@ export const getCurrentUser = async () => {
 
     return parseStringify(user.documents[0]);
   
+};
+
+
+export const signOutUser = async () => {
+  const { account } = await createSessionClient();
+
+  try {
+    await account.deleteSession("current");
+    (await cookies()).delete("appwrite-session");
+  } catch (error) {
+    handleError(error, "Failed to sign out user");
+  } finally {
+    redirect("/sign-in");
+  }
 };
